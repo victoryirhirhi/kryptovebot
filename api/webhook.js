48037f157ec5520c34cd7dc78ca0fcb57ee142d9
victoryirhirhi <45254725+victoryirhirhi@users.noreply.github.com>
2025-09-23 05:46:54 +0100

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,10 +1,24 @@
 import bot from "../../bot/bot.js";
 
+export const config = {
+  api: {
+    bodyParser: false, // disable automatic body parsing
+  },
+};
+
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
-      await bot.handleUpdate(req.body);
-      return res.status(200).send("OK");
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on("end", async () => {
+        const update = JSON.parse(body);
+        await bot.handleUpdate(update);
+        res.status(200).send("OK");
+      });
     } catch (err) {
       console.error("Webhook error:", err);
       return res.status(500).send("Error");
