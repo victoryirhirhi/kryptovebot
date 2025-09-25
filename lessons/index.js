2a922711929a0a01baa4120635dad0e1dd69b2fc
victoryirhirhi <45254725+victoryirhirhi@users.noreply.github.com>
2025-09-25 13:15:44 +0100

    Update index.js

--- lessons/index.js
+++ lessons/index.js
@@ -1,20 +1,9 @@
-// lessons/index.js
-
-import * as Trading from "./trading.js";
-import * as Web3Job from "./web3job.js";
-import * as Funding from "./funding.js";
+import { tradingLessons, tradingQuiz } from "./trading.js";
+import { web3JobLessons, web3JobQuiz } from "./web3job.js";
+import { fundingLessons, fundingQuiz } from "./funding.js";
 
 export const LESSONS = {
-  trading: {
-    lessons: Trading.tradingLessons || [],
-    quiz: Trading.tradingQuiz || {}
-  },
-  web3job: {
-    lessons: Web3Job.web3JobLessons || [],
-    quiz: Web3Job.web3JobQuiz || {}
-  },
-  funding: {
-    lessons: Funding.fundingLessons || [],
-    quiz: Funding.fundingQuiz || {}
-  }
+  trading: { lessons: tradingLessons, quiz: tradingQuiz },
+  web3job: { lessons: web3JobLessons, quiz: web3JobQuiz },
+  funding: { lessons: fundingLessons, quiz: fundingQuiz }
 };
