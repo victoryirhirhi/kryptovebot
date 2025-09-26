4340d41d35cad20103593b8c6f6a6eca5301f0dc
victoryirhirhi <45254725+victoryirhirhi@users.noreply.github.com>
2025-09-26 20:27:53 +0100

    Update trading.js

--- lessons/trading.js
+++ lessons/trading.js
@@ -4,17 +4,17 @@ import { noviceLessons, noviceQuiz } from "./noviceLessons.js";
 import { intermediateLessons, intermediateQuiz } from "./intermediateLessons.js";
 import { professionalLessons, professionalQuiz } from "./professionalLessons.js";
 
-// Each lesson must keep { title, content } format
-// Each quiz must keep { question, options[], answer } format
-
 export const tradingLessons = [
   ...noviceLessons,
   ...intermediateLessons,
   ...professionalLessons,
 ];
 
-export const tradingQuiz = [
-  ...noviceQuiz,
-  ...intermediateQuiz,
-  ...professionalQuiz,
-];
+export const tradingQuiz = {
+  questions: [
+    ...noviceQuiz.questions,
+    ...intermediateQuiz.questions,
+    ...professionalQuiz.questions,
+  ],
+  passingScore: 70, // you can set this higher if needed
+};
